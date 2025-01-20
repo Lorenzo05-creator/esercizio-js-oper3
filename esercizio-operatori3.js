29db0d1c55c45479d34d228b413e05c171059bf1
Lorenzo05-creator <lory4jz@gmail.com>
2025-01-20 14:17:50 +0100

    Add files via upload

---
+++ esercizio-operatori3.js
@@ -0,0 +1,10 @@
+let number1 = prompt(`Dimmi un numero a tua scelta`);
+let number2 = prompt(`Dimmi un altro numero a tua scelta`);
+
+let somma = number1 + number2;
+let sottrazione = number1 - number2;
+let moltiplicazione = number1 * number2;
+let divisione = number1 / number2;
+let potenza = Math.pow(number1, number2);
+
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: somma: ${somma}, sottrazione: ${sottrazione}, moltiplicazione: ${moltiplicazione}, divisione: ${divisione}, potenza: ${potenza}`);
